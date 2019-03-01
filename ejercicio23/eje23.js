d74b0211d57c49e21f9ce9a150c1e9a59610f843
Maybol <maybol.huracahua@tecsup.edu.pe>
2019-03-01 10:40:08 -0500

    ejercicio23
    ejercicio23

---
+++ ejercicio23/eje23.js
@@ -0,0 +1,8 @@
+console.log("esta es la consola de cama arena")
+var huespedN=20;
+console.log("El huesped como referencia pagara 20 por la noche")
+var huespedD=20;
+console.log("El huesped como referencia pagara 20 por el dia")
+var estancia=huespedD+huespedN;
+console.log("El huesped pagara por su estancia dependiendo a la hora de ingreso 20 por la noche y si continua 20 por el dia ")
+console.log("Lo que se cobrara a este huesped es"+estancia)

0929137e26670bafa62a673eb84cf7f4b86d4e3e
Eduardo de Rivero <ederiveroman@gmail.com>
2019-03-01 09:43:08 -0500

    agregue javascrip

---
+++ 01.variables.js
@@ -0,0 +1,46 @@
+//01
+var base=3,
+altura=4,
+area=(base*altura)/2;
+console.log("El Area es: "+area);
+//02
+var pesos=1000,
+tc=0.05186,
+resultado=pesos*tc;
+console.log("El cambio de "+pesos+" pesos es: $"+resultado);
+//3
+var anio=1992,
+anio_actual=2019,
+resultado=anio_actual-anio;
+console.log("La persona tiene "+resultado+" años");
+//04
+var costoxmin=0.15,
+totaltiempo=80,
+resultado=costoxmin*totaltiempo;
+console.log("La persona debe pagar "+resultado);
+//05
+var area=10,
+precioxm=10,
+resultado=area*precioxm;
+console.log("El precio es "+resultado)
+//06
+var ladoa=5,
+ladob=8,
+hipotenusa=Math.sqrt(ladoa*ladob);
+console.log("La hipotenusa es "+hipotenusa);
+//07
+var distanciakm=1000,
+precioxkm=5,
+boleto=distancia*precioxkm;
+console.log("El boleto para una distancia de "+distanciakm+"km es de "+boleto);
+//08
+var velocidadkmxh = 10,
+distanciakm=20,
+totaltiempo=distanciakm/velocidadkmxh;
+console.log("El tiempo que demorara para una distancia de "+distanciakm+"km con una velocidad de "+velocidadkmxh+" es de "+totaltiempo);
+//09
+var tiempollamada= 40,
+costoxmin=0.5,
+costollamada=costoxmin*tiempollamada;
+console.log("El costo de llamada es "+costollamada+" para una llamada de "+tiempollamada+"minutos");
+

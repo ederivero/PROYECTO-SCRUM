f19819965908b2bb0063e1ea7e154c906b889e6c
Yuleymisr <yuleymi.sihuinta@tecsup.edu.pe>
2019-03-01 17:29:46 -0500

    Ejercicios 4, 5 y 6

--- joel/ejercicios lunes.js
+++ joel/ejercicios lunes.js
@@ -32,6 +32,92 @@ if (Presupuesto > 100 && Presupuesto < 251){
 if (Presupuesto > 251){
     console.log("compra anillo")
 }
+
+
+//  Ejercicio 4
+
+var horas = 6;
+
+if ((horas >= 0) && (horas <= 2)) {
+    var c_1 = horas*5;
+            console.log("La tarifa por " + horas+ " es: " + c_1 + "pesos");
+} else {
+    if ((horas >= 3)&&(horas < 6 )) {
+        var c_2 = horas*4;
+        console.log("La tarifa por " + horas+ " es: " + c_2 + "pesos");
+    } else {
+        if  ((horas >= 6)&&(horas < 11 )) {
+            var c_3 = horas*3;
+            console.log("La tarifa por " + horas+ " es: " + c_3 + "pesos");
+        } else {
+            if (horas >= 11) {
+                var c_4 = horas*2;
+                console.log("La tarifa por " + horas+ " es: " + c_4 + "pesos");
+            } else {
+                console.log("Cantidad de horas invalida");
+                
+            }
+        }
+    }    
+}
+
+// Ejercicio 5
+
+var a = "mauricio";
+var d = 27;
+
+var b = "carlos";
+var e = 89;
+
+var c = "eder";
+var f = 1;
+
+if ((f<=0) || (e<=0) || (d<=0) ){
+    console.log("Ingreso una edad invalida");
+} else {
+    if ((f>=e)&&(e>=d)) {
+        console.log("El menor es " + a);
+    } else {
+        if ((e>=d)&&(d>=f)) {
+            console.log("El menor es " + c);
+        } else {
+            if ((d>=f)&&(f>=e)) {
+                console.log("El menor es " + b);
+            }        
+        }
+    }
+}
+
+// Ejercicio 6
+
+var p = 210;
+
+
+if (p<0) {
+    console.log("Precio Invalido");
+} else {
+    if ((p>=200)) {
+        var p_desc = p - (p*0.15);
+        console.log("El precio es "+ p +" su precio con descuento es " + p_desc);
+    } else {
+       if ((p>100)&&(p<200)) {
+        var p_desc = p - (p*0.12);
+        console.log("El precio es "+ p +" su precio con descuento es " + p_desc);  
+       } else {
+           if (p<100) {
+            var p_desc = p - (p*0.10); 
+            console.log("El precio es "+ p +" su precio con descuento es " + p_desc);  
+             } else{
+                 console.log("El precio es de " + p);
+                 
+            }
+        }
+    }
+}
+
+
+
+
 // 16
 console.log("ejercicio 16");
 var puntos = 150;

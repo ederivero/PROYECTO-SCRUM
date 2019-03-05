ff78c5aee346ed365d2512e0cf312eb9f1f0d647
VanessaMMH <45247476+VanessaMMH@users.noreply.github.com>
2019-03-05 17:29:11 -0500

    Update Vanesa Ej-3-4.js

--- SCRUM 05-03-2019/Vanesa Ej-3-4.js
+++ SCRUM 05-03-2019/Vanesa Ej-3-4.js
@@ -16,15 +16,21 @@ var n;
 n=+prompt(`######  Ejercicio 4 #####
 Ingrese un numero:`)
 
+//var abecedario=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+//console.log(abecedario[n-1]);
+
+var caracter=String.fromCharCode(n+64) ;
+var valor=n+64;
+
+
 switch(true){
     case n==1 || n==5 || n==9 || n==15 || n==21:
-        alert("=> Es una vocal");
+        alert(caracter+"=> Es una vocal");
         break;
     case n>0 && n<27:
-        alert("=> Es una consonante");
+        alert(caracter+"=> Es una consonante");
         break;
     default:
-        alert(n+" => Es un numero invalido")
+        alert(caracter+" => No es consonante ni vocal");
 
 }
-

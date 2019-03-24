6c839519a3b74c08857d6d74d9f8b3efa3804cc5
ederivero <ederiveroman@gmail.com>
2019-03-24 17:57:50 -0500

    Ya hice todo el banco, un saludo
    LEANLO POR SI EL PROFE PREGUNTA JAJA

---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Banco
+{
+    class Program
+    {
+        static Program p = new Program();
+        static Transaccion transaccion = new Transaccion();
+        public static List<string> Ldni = new List<string>();
+        public static List<string> Lnombre = new List<string>();
+        public static List<string> Lapellido = new List<string>();
+        public static List<string> Lcuenta = new List<string>();
+        public static List<string> Lmonto = new List<string>();
+        public static List<string> Lmoneda = new List<string>();
+        public static string url = "C:\\Users\\ederi\\OneDrive\\Documentos\\GitHub\\PROYECTO-SCRUM\\bd.txt";
+        public static string url2 = "C:\\Users\\ederi\\OneDrive\\Documentos\\GitHub\\PROYECTO-SCRUM\\bd(1).txt";
+
+        static void Main(string[] args)
+        {
+            LeerArchivo();
+            string rpta = "";
+            Console.WriteLine("INGRESE SU DNI");
+            string dni = Console.ReadLine();
+            rpta = EncontrarCliente(dni);
+            if (rpta != "")
+            {
+                Menu(rpta);
+            }
+        }
+        private static void LeerArchivo()
+        {
+            string[] strLineas = File.ReadAllLines(url);
+            string[] campos;
+            for (int i = 1; i < strLineas.Length; i++)
+            {
+                campos=strLineas[i].Split(",".ToCharArray());
+                Ldni.Add(campos[0]);
+                Lnombre.Add(campos[1]);
+                Lapellido.Add(campos[2]);
+                Lcuenta.Add(campos[3]);
+                Lmonto.Add(campos[4]);
+                Lmoneda.Add(campos[5]);
+            }
+        }
+
+        private static string EncontrarCliente(string DNI)
+        {
+            int i = 0;
+            foreach (var item in Ldni)
+            {
+                if(item==DNI)
+                {
+                    Console.WriteLine($"BIENVENIDO {Lnombre[i]}");
+                    return DNI;
+                }
+                
+                i++;
+            }
+            Console.WriteLine("CLIENTE NO ENCONTRADO");
+            return "";
+        }
+
+        static void Menu(string DNI)
+        {
+            int opc;
+            do
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("-  CAJERO AUTOMATICO PANBIF -");
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("1. CONSULTAR SALDO");
+                Console.WriteLine("2. DEPOSITAR");
+                Console.WriteLine("3. RETIRAR");
+                Console.WriteLine("4. TRANSFERENCIA");
+                Console.WriteLine("0. TERMINAR");
+                Console.WriteLine("INGRESE OPCION");
+                opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 1:
+                        transaccion.Consultar(DNI);
+                        break;
+                    case 2:
+                        transaccion.Depositar(DNI);
+                        break;
+                    case 3:
+                        transaccion.Retirar(DNI);
+                        break;
+                    case 4:
+                        transaccion.Transferir(DNI);
+                        break;
+                    case 0:
+
+                        break;
+                    default:
+                        Console.WriteLine("-  OPCION INCORRECTA  -");
+                        break;
+
+                }
+            } while (opc != 0);
+
+        }
+        public static void Guardar()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(url, false, Encoding.ASCII);
+                sw.WriteLine("dni,nombres,apellidos,cuenta,monto,moneda");
+                int i = 0;
+                foreach (var item in Ldni)
+                {
+                    sw.WriteLine(Ldni[i]+","+Lnombre[i] + "," +Lapellido[i] + "," +Lcuenta[i] + "," +Lmonto[i] + "," +Lmoneda[i]);
+                    i++;
+                }
+                sw.Close();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR AL GUARDAR ARCHIVO :/");
+
+            }
+        }
+
+    }
+}
---
+++ Banco/Banco/Transaccion.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco
+{
+    class Transaccion
+    {
+        //Program p = new Program();
+        List<string> cliente = new List<string>();
+        List<string> cuentas = new List<string>();
+        List<string> moneda = new List<string>();
+        List<double> monto = new List<double>();
+        List<int> posiciones = new List<int>();
+        private int IniciarListas(string DNI)
+        {
+            int i = 0, j = 1;
+            foreach (var item in Program.Ldni)
+            {
+                if (item == DNI)
+                {
+                    Console.WriteLine($"[{j}] {Program.Lcuenta[i]} MONEDA-> {Program.Lmoneda[i]}");
+                    cliente.Add(Program.Lnombre[i] + " " + Program.Lapellido[i]);
+                    cuentas.Add(Program.Lcuenta[i]);
+                    moneda.Add(Program.Lmoneda[i]);
+                    monto.Add(double.Parse(Program.Lmonto[i]));
+                    posiciones.Add(i);
+                    j++;
+                }
+                i++;
+            }
+            Console.Write("SELECCIONE CUENTA: ");
+            return j;
+        }
+        private void Guardar(int nro,string DNI)
+        {
+            Program.Lmonto[posiciones[nro - 1]] = monto[nro - 1].ToString();
+            Program.Guardar();
+            IniciarListas(DNI);
+
+            Console.WriteLine($"CLIENTE: {Program.Lnombre[posiciones[nro - 1]]} {Program.Lapellido[posiciones[nro - 1]]}");
+            Console.WriteLine($"SALDO ACTUAL: {Program.Lmonto[posiciones[nro - 1]]} MONEDA: {Program.Lmoneda[posiciones[nro - 1]]}");
+        }
+        public void Consultar(string DNI)
+        {
+            int j = IniciarListas(DNI);
+            try
+            {
+
+                int cta = int.Parse(Console.ReadLine());
+                if (cta < j && cta > 0)
+                {
+                    Console.WriteLine($"CLIENTE: {cliente[cta - 1]}");
+                    Console.WriteLine($"SALDO ACTUAL: {monto[cta - 1]} MONEDA: {moneda[cta - 1]}");
+                }
+                else
+                {
+                    Console.WriteLine("****** ERROR DE CUENTA");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("****** ERROR DE CUENTA");
+            }
+
+        }
+        public void Depositar(string DNI)
+        {
+            int j = IniciarListas(DNI);
+            try
+            {
+
+                int cta = int.Parse(Console.ReadLine());
+                if (cta < j && cta > 0)
+                {
+                    Console.Write("INGRESE MONTO: ");
+                    try
+                    {
+                        double montos = double.Parse(Console.ReadLine());
+                        if (montos < 0)
+                        {
+                            Console.WriteLine("****** MONTO NEGATIVO");
+                        }
+                        else
+                        {
+                            monto[cta - 1] = monto[cta - 1] + montos;
+                            Guardar(cta,DNI);
+                        }
+
+
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("****** ERROR MONTO INCORRECTO");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("****** ERROR DE CUENTA");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("****** ERROR DE CUENTA");
+            }
+
+        }
+        public void Retirar(string DNI)
+        {
+            int j = IniciarListas(DNI);
+            try
+            {
+                int cta = int.Parse(Console.ReadLine());
+                if (cta < j && cta > 0)
+                {
+                    Console.Write("INGRESE MONTO: ");
+                    try
+                    {
+                        double montos = double.Parse(Console.ReadLine());
+                        if (montos > monto[cta - 1])
+                        {
+                            Console.WriteLine("****** MONTO MAS DE LO QUE TIENE");
+                        }
+                        else
+                        {
+                            monto[cta - 1] = monto[cta - 1] - montos;
+                            Guardar(cta,DNI);
+                        }
+
+
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("****** ERROR MONTO INCORRECTO");
+                    }
+
+                    Console.WriteLine($"CLIENTE: {cliente[cta - 1]}");
+                    Console.WriteLine($"SALDO ACTUAL: {monto[cta - 1]} MONEDA: {moneda[cta - 1]}");
+                }
+                else
+                {
+                    Console.WriteLine("****** ERROR DE CUENTA");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("****** ERROR DE CUENTA");
+            }
+
+
+        }
+        public void Transferir(string DNI)
+        {
+            string destino = "";
+            int j = IniciarListas(DNI);
+            try
+            {
+                int i=0, cta = int.Parse(Console.ReadLine());
+                Console.Write("INGRESE CUENTA DE DESTINO: ");
+                destino = Console.ReadLine();
+
+                foreach (var item in Program.Lcuenta)
+                {
+                    if (destino == item)
+                    {
+                        if (moneda[cta - 1] == Program.Lmoneda[i])
+                        {
+                            Console.WriteLine($"CLIENTE DESTINO: {Program.Lnombre[i]} {Program.Lapellido[i]}");
+                            Console.Write("MONTO: ");
+                            double monto1 = double.Parse(Console.ReadLine());
+                            if (monto1 > 0)
+                            {
+                                if(monto1< monto[cta - 1])
+                                {
+                                    Program.Lmonto[i]= (double.Parse(Program.Lmonto[i])+monto1).ToString();
+                                    monto[cta - 1] = monto[cta - 1] - monto1;
+                                    Guardar(cta,DNI);
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("****** ERROR SALDO INSUFICIENTE");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("****** ERROR MONTO NEGATIVO");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("****** Error cuenta destino esta en otra moneda");
+                        }
+                    }
+                    i++;
+                }
+
+            }
+            catch (Exception) {
+                Console.WriteLine("****** ERROR DE CUENTA");                
+
+            }
+        }
+
+    }
+}
